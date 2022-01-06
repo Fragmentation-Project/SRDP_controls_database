77e34f9ede303ebbc9543f8a4aa2070ae69d0a05
Harriet Goers <h.goers@hotmail.com>
2022-01-06 12:20:03 -0500

    Create full dataset combining group and country levels

---
+++ munge/munge_full_dataset.R
@@ -0,0 +1,12 @@
+# This script combines all group- and country-level data.
+
+library(tidyverse)
+library(rio)
+library(here)
+
+full_df <- import(here("data", "group_level.rds")) %>% 
+  left_join(import(here("data", "country_level.rds"))) %>% 
+  arrange(kgcid, year)
+
+export(full_df, here("data", "full_dataset.rds"))
+export(full_df, here("data", "full_dataset.csv"))
