6280e9fbb2c0b565ab01389ff85271d2bbbe16c3
Harriet Goers <h.goers@hotmail.com>
2022-08-30 15:43:20 -0400

    Clean up group sourcing

--- src/src_group_level.R
+++ src/src_group_level.R
@@ -6,9 +6,14 @@ library(rio)
 # Group scope
 groups_raw <- sRdpPrivateData::groups
 
+rio::export(groups_raw, here::here("data-raw", "groups_raw.csv"))
+
 # Size and relative size
-relative_size_raw <- import("https://icr.ethz.ch/data/epr/core/EPR-2021.csv")
+relative_size_raw <- rio::import("https://icr.ethz.ch/data/epr/core/EPR-2021.csv")
+
+rio::export(relative_size_raw, here::here("data-raw", "relative_size_raw.csv"))
 
 # Religion
-religion_raw <- googlesheets4::range_read("1Q6jeejbdmoNmnQFgma_BgH8BtEBdkFHzSsNBdLPomWY") %>% 
-  janitor::clean_names()
+religion_raw <- googlesheets4::range_read("1Q6jeejbdmoNmnQFgma_BgH8BtEBdkFHzSsNBdLPomWY")
+
+rio::export(religion_raw, here::here("data-raw", "religion_raw.csv"))
